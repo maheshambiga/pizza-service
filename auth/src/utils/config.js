047500cea5c8fedb04e8fda6fe218de017afb3bb
Mahesh Ambig <mahambig@publicisgroupe.net>
2020-04-13 20:07:59 -0400

    load dotenv only in dev mode

--- auth/src/utils/config.js
+++ auth/src/utils/config.js
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const envFound = dotenv.config();
-if (!envFound) {
-    // This error should crash whole process
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+
+if (process.env.NODE_ENV !== 'production') {
+    const envFound = dotenv.config();
+    if (!envFound) {
+        // This error should crash whole process
+        throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    }
 }
 
 export default {
